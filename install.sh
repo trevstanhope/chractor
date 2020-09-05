4245a07e4c2a0deff4dcfd55ed8447cd5bf441f3
Trevor Stanhope <tpstanhope@gmail.com>
2020-09-04 22:38:48 -0500

    cleaning up installer

--- install.sh
+++ install.sh
@@ -8,18 +8,6 @@ CONFIG_PATH="$PWD/conf"
 BIN_PATH="$PWD/bin"
 BUILD_PATH="$PWD/build"
 
-# Update Kernel
-read -p "Update kernel [y/n]? " ans
-if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
-    then
-        cd $CONFIG_PATH/kernel
-        dpkg -i linux-headers-4.2.0*.deb linux-image-4.2.0*.deb
-fi
-if [ $ans = n -o $ans = N -o $ans = no -o $ans = No -o $ans = NO ]
-    then
-        echo "Skipping..."
-fi
-
 # GRUB
 read -p "Enable fast GRUB? [y/n] " ans
 if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
@@ -54,70 +42,10 @@ fi
 read -p "Update APT dependencies? [y/n] " ans
 if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
     then
-        echo "Installing dependencies via mirror ..."
+        echo "Installing APT dependencies ..."
         cp $CONFIG_PATH/sources.list /etc/apt/
         apt-get update
         apt-get upgrade -y -qq
-        apt-get install unzip -y -qq
-        apt-get install build-essential -y -qq
-        apt-get install python-dev -y -qq
-        apt-get install cmake -y -qq
-        apt-get install python-serial -y -qq
-        apt-get install python-pip -y -qq
-        apt-get install python-gps -y -qq # python dependencies
-        apt-get install python-zmq -y -qq
-        apt-get install mongodb -y -qq # MongoDB
-        apt-get install gpsd -y -qq
-        apt-get install gpsd-clients -y -qq
-        apt-get install python-gps -y  -qq # GPS
-        apt-get install python-matplotlib -y -qq
-        apt-get install libgtk2.0-dev -y -qq
-        apt-get install python-numpy -y -qq
-        apt-get install libqt4-dev -y -qq
-        apt-get install libopencv-dev -y -qq
-        apt-get install build-essential -y -qq
-        apt-get install checkinstall -y -qq
-        apt-get install pkg-config -y -qq
-        apt-get install yasm -y -qq
-        apt-get install libjpeg-dev -y -qq
-        apt-get install libjasper-dev -y -qq
-        apt-get install libavcodec-dev -y -qq
-        apt-get install libavformat-dev -y -qq
-        apt-get install libswscale-dev -y -qq
-        apt-get install libdc1394-22-dev -y -qq
-        apt-get install libxine2-dev -y -qq
-        apt-get install libav-tools -y -qq
-        apt-get install libgstreamer0.10-dev -y -qq
-        apt-get install libgstreamer-plugins-base0.10-dev -y -qq
-        apt-get install libv4l-dev -y -qq
-        apt-get install python-numpy -y -qq
-        apt-get install libtbb-dev -y -qq
-        apt-get install libqt4-dev -y -qq
-        apt-get install libgtk2.0-dev -y -qq
-        apt-get install libfaac-dev -y -qq
-        apt-get install libmp3lame-dev -y -qq
-        apt-get install libopencore-amrnb-dev -y -qq
-        apt-get install libopencore-amrwb-dev -y -qq
-        apt-get install libtheora-dev -y -qq
-        apt-get install libvorbis-dev -y -qq
-        apt-get install libxvidcore-dev -y -qq
-        apt-get install x264 -y -qq
-        apt-get install v4l-utils -y -qq
-        apt-get install arduino arduino-mk -qq
-        apt-get install x11-xserver-utils -y -qq
-        apt-get install gcc -y -qq
-        apt-get install gfortran -y -qq
-        apt-get install libblas-dev -y -qq
-        apt-get install liblapack-dev -y -qq
-        apt-get install cython -y -qq
-        apt-get install hostapd -y -qq
-        apt-get install dnsmasq -y -qq
-        apt-get install curl -y -qq
-        apt-get install automake -y -qq
-        apt-get install autoconf -y -qq
-        apt-get install libtool -y -qq
-        apt-get install pkg-config -y -qq
-        apt-get -y install autoconf automake build-essential libass-dev libfreetype6-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev
 fi
 if [ $ans = n -o $ans = N -o $ans = no -o $ans = No -o $ans = NO ]
     then
@@ -125,7 +53,7 @@ if [ $ans = n -o $ans = N -o $ans = no -o $ans = No -o $ans = NO ]
 fi
 
 # Pip Packages
-read -p "Update Pip modules? [y/n] " ans
+read -p "Update Python modules? [y/n] " ans
 if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
     then
         echo "Installing Python modules..."
@@ -142,7 +70,7 @@ if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
     then
         echo "Adding daemon to init.d"
         cp $CONFIG_PATH/app /etc/init.d/
-        chmod +x /etc/init.d/mutrac
+        chmod +x /etc/init.d/chractor
         chown root:root /etc/init.d/app
         update-rc.d app defaults
         update-rc.d app enable
