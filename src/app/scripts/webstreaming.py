d514e3811edbac0fb1e44eec7f241891da0cfada
Trevor Stanhope <tpstanhope@gmail.com>
2020-09-07 23:01:29 -0500

    v2 ready to test on ECU

--- src/app/scripts/webstreaming.py
+++ src/app/scripts/webstreaming.py
@@ -13,12 +13,10 @@ import cv2
 
 outputFrame = None
 lock = threading.Lock()
-# initialize a flask object
-app = Flask(__name__)
-# initialize the video stream and allow the camera sensor to
-# warmup
-#vs = VideoStream(usePiCamera=1).start()
-vs = VideoStream("rtp://192.168.40.181:50008").start()
+app = Flask(__name__) # initialize a flask object
+# initialize the video stream and allow the camera sensor to  warmup
+#vs = VideoStream("rtp://192.168.40.181:50008").start()
+vs = VideoStream(0).start()
 time.sleep(2.0)
 
 @app.route("/")
@@ -45,23 +43,23 @@ def detect_motion(frameCount):
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		gray = cv2.GaussianBlur(gray, (7, 7), 0)
 		# grab the current timestamp and draw it on the frame
-		timestamp = datetime.datetime.now()
-		cv2.putText(frame, timestamp.strftime(
-			"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+		#timestamp = datetime.datetime.now()
+		#cv2.putText(frame, timestamp.strftime(
+		#	"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+		#	cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 		# if the total number of frames has reached a sufficient
 		# number to construct a reasonable background model, then
 		# continue to process the frame
 		if total > frameCount:
-			# detect motion in the image
+			#	# detect motion in the image
 			motion = md.detect(gray)
-			# check to see if motion was found in the frame
-			if motion is not None:
-				# unpack the tuple and draw the box surrounding the
-				# "motion area" on the output frame
-				(thresh, (minX, minY, maxX, maxY)) = motion
-				cv2.rectangle(frame, (minX, minY), (maxX, maxY),
-					(0, 0, 255), 2)
+			#	# check to see if motion was found in the frame
+			#	if motion is not None:
+			#		# unpack the tuple and draw the box surrounding the
+			#		# "motion area" on the output frame
+			#		(thresh, (minX, minY, maxX, maxY)) = motion
+			#		cv2.rectangle(frame, (minX, minY), (maxX, maxY),
+			#			(0, 0, 255), 2)
 		
 		# update the background model and increment the total number
 		# of frames read thus far
@@ -89,35 +87,28 @@ def generate():
 			if not flag:
 				continue
 		# yield the output frame in the byte format
-		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
-			bytearray(encodedImage) + b'\r\n')
+		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
 
 @app.route("/video_feed")
 def video_feed():
 	# return the response generated along with the specific media
 	# type (mime type)
-	return Response(generate(),
-		mimetype = "multipart/x-mixed-replace; boundary=frame")
+	return Response(generate(), mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 # check to see if this is the main thread of execution
 if __name__ == '__main__':
 	# construct the argument parser and parse command line arguments
 	ap = argparse.ArgumentParser()
-	ap.add_argument("-i", "--ip", type=str, required=True,
-		help="ip address of the device")
-	ap.add_argument("-o", "--port", type=int, required=True,
-		help="ephemeral port number of the server (1024 to 65535)")
-	ap.add_argument("-f", "--frame-count", type=int, default=32,
-		help="# of frames used to construct the background model")
+	ap.add_argument("-i", "--ip", type=str, required=True, help="ip address of the device")
+	ap.add_argument("-o", "--port", type=int, required=True, help="ephemeral port number of the server (1024 to 65535)")
+	ap.add_argument("-f", "--frame-count", type=int, default=32, help="# of frames used to construct the background model")
 	args = vars(ap.parse_args())
 	# start a thread that will perform motion detection
-	t = threading.Thread(target=detect_motion, args=(
-		args["frame_count"],))
+	t = threading.Thread(target=detect_motion, args=(args["frame_count"],))
 	t.daemon = True
 	t.start()
 	# start the flask app
-	app.run(host=args["ip"], port=args["port"], debug=True,
-		threaded=True, use_reloader=False)
+	app.run(host=args["ip"], port=args["port"], debug=True, threaded=True, use_reloader=False)
 
 # release the video stream pointer
 vs.stop()
